cf358533f88128a9e3d6fcbb4f8b0cfb36571d58
Ansarissab <zahidensari116@gmail.com>
2024-03-08 21:13:17 +0500

    Initial commit

---
+++ content.js
@@ -0,0 +1,34 @@
+const sidebar = document.querySelector("div._2Ts6i div#side");
+const buttonContainer = document.createElement("div");
+const button = document.createElement("button");
+
+buttonContainer.classList.add("sidebar-toggle-container");
+button.classList.add("sidebar-toggle-button");
+
+// Set initial button text and sidebar visibility based on preference (optional)
+let isSidebarHidden = false; // Change to true to hide sidebar initially
+
+if (isSidebarHidden) {
+  sidebar.style.display = "none";
+  button.textContent = ">";
+} else {
+  button.textContent = "<";
+}
+
+buttonContainer.appendChild(button);
+document.body.appendChild(buttonContainer);
+
+button.addEventListener("click", () => {
+  const sidebar = document.querySelector("div._2Ts6i div#side");
+
+  isSidebarHidden = !isSidebarHidden;
+  if (isSidebarHidden) {
+    sidebar.style.display = "none";
+    sidebar.parentNode.style.display = "none";
+    button.textContent = ">";
+  } else {
+    sidebar.style.display = ""; // Reset to default style
+    sidebar.parentNode.style.display = "";
+    button.textContent = "<";
+  }
+});
